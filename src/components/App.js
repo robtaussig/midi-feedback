c4441d0fcd22af538cb9fb6461f137607f867151
Gregory Nelson Tippett <gntsketches@gmail.com>
2019-05-12 20:11:13 -0700

    first working draft - app now downloads MIDI from QWERTY!

--- src/components/App.js
+++ src/components/App.js
@@ -4,33 +4,50 @@ import { connect } from 'react-redux'
 import { toggleRecording } from "../actions"
 
 import PressQwerty from './pressQwerty'
+import { quantizations } from '../config/constants'
+
 // import Header from './components/header'
 // import Qwerty from './components/Qwerty';
 
 import './App.css';
 
-const Tone = window.Tone;
 const scribble = window.scribble;
+const Tone = window.Tone;
+const metronome = new Tone.MembraneSynth({
+	'octaves': 4,
+}).toMaster()
+// const player = new Tone.Player({
+// 	'url': './public/resources/sounds/woodblock.mp3'
+// }).toMaster()
+// const player = new Tone.Player('../../public/resources/sounds/woodblock.mp3').toMaster()
+// console.log(player)
 
-const c = scribble.clip({
-	notes: 'c4', // try 'c4- ! gives note: ["C4", "F4", "Bb4", "Eb5"]
-	pattern: 'x_--'
-});
+	Tone.Transport.scheduleRepeat(function(time) {
+		// player.start(time)
+		metronome.triggerAttackRelease('C2', '8n')
+	}, "4n")
 
-const bhang = scribble.clip({
-	notes: 'b4',
-	pattern: 'x'
-});
 
+
+// const c = scribble.clip({
+// 	notes: 'c4', // try 'c4- ! gives note: ["C4", "F4", "Bb4", "Eb5"]
+// 	pattern: 'x_'
+// })
+//
+// const bhang = scribble.clip({
+// 	notes: 'b4',
+// 	pattern: 'x'
+// });
+//
 // console.log([...c, ...bhang])
 
-const bytes = scribble.midi([...c, ...bhang], null); // Pass `null` as the second param to get bytes
-const b64 = btoa(bytes); // Encode byte string from Scribbletune as base64
-const uri = 'data:audio/midi;base64,' + b64;
-const link = document.createElement('a');
-
-link.href = uri;
-link.download = 'music.mid';
+// const bytes = scribble.midi([...c, ...bhang], null); // Pass `null` as the second param to get bytes
+// const b64 = btoa(bytes); // Encode byte string from Scribbletune as base64
+// const uri = 'data:audio/midi;base64,' + b64;
+// const link = document.createElement('a');
+//
+// link.href = uri;
+// link.download = 'music.mid';
 
 
 class App extends React.Component {
@@ -51,10 +68,76 @@ class App extends React.Component {
 	downloadMidi = () => {
 		console.log(this.props.tune)
 
-		// let tune = [...this.props.tune].sort()
-			// https://www.geeksforgeeks.org/javascript-array-sort/
+		let tune = [...this.props.tune]
+		tune.sort((a,b)=>{
+			return a.startTime - b.startTime
+		})
+		tune.forEach((noteObj, index) => {
+			if (tune[index+1] && noteObj.endTime > tune[index+1].startTime) {
+				noteObj.endTime = tune[index+1].startTime
+			}
+		})
+		console.log(tune)
+		const quantizationConverter = quantizations[this.props.quantization]/2
+		console.log(quantizationConverter)
 
-		// link.click(); // this will start a download of the MIDI byte string as a file called "music.mid"
+		let quantizedTune = []
+		tune.forEach((noteObj, index) => {
+			const quantizedNote = { note: noteObj.note}
+			quantizedNote.startBeat = Math.round(noteObj.startTime / quantizationConverter)
+			quantizedNote.endBeat = Math.round(noteObj.endTime / quantizationConverter)
+			quantizedTune.push(quantizedNote)
+		})
+		console.log(quantizedTune)
+
+		// const first = scribble.clip({
+		// 	notes: quantizedTune[0].note,
+		// 	pattern: 'x'
+		// });
+
+		function buildScribblePattern(played, count) {
+			let pattern = ''
+			if (played) {
+				pattern = 'x'
+				for (let i = 1; i < count; i++) {
+					pattern += '_'
+				}
+			} else {
+				for (let i = 0; i < count; i++) {
+					pattern += '-'
+				}
+			}
+			return pattern
+		}
+
+		let midiTune = []
+
+		quantizedTune.forEach((noteObj, index) => {
+
+			let pattern = buildScribblePattern(true, noteObj.endBeat - noteObj.startBeat)
+			if (quantizedTune[index+1] && quantizedTune[index+1].startBeat - noteObj.endBeat > 0) {
+				pattern += buildScribblePattern(false, quantizedTune[index+1].startBeat - noteObj.endBeat)
+			}
+
+			const clip = scribble.clip({
+				notes: noteObj.note,
+				pattern: pattern
+			})
+
+			midiTune = [...midiTune, ...clip]
+
+		})
+		console.log(midiTune)
+
+		const bytes = scribble.midi(midiTune, null); // Pass `null` as the second param to get bytes
+		const b64 = btoa(bytes); // Encode byte string from Scribbletune as base64
+		const uri = 'data:audio/midi;base64,' + b64;
+		const link = document.createElement('a');
+
+		link.href = uri;
+		link.download = 'music.mid';
+
+		link.click(); // this will start a download of the MIDI byte string as a file called "music.mid"
 	}
 
 	render() {
@@ -74,7 +157,8 @@ class App extends React.Component {
 const mapStateToProps = (state) => {
 	return {
 		isRecording: state.isRecording,
-		tune: state.tune
+		tune: state.tune,
+		quantization: state.quantization
 	}
 }
 
--- src/components/pressQwerty.js
+++ src/components/pressQwerty.js
@@ -6,10 +6,18 @@ import { addNoteToTune } from '../actions'
 const PressQwerty = (props) => {
 
 	const noteKeys_All = ["a", "z", "s", "x", "d", "c", "f", "v", "g", "b", "h", "n", "j", "m", "k", ",", "l", ".", ";", "/", "'", "1", "q", "2", "w", "3", "4", "e", "r", "5", "t", "6", "y", "7", "u", "8", "i", "9", "o", "0", "p", "-", "[", "=", "]", "Backspace", "\\"]
-	const keyToPitch = {" ":" ", "z":"C3", "s":"C#3", "x":"D3", "d":"D#3", "c":"E3", "v":"F3", "g":"F#3", "b":"G3", "h":"G#3", "n":"A3", "j":"A#3", "m":"B3", ",":"C4", "q":"C4", "2":"C#4", "w":"D4", "3":"D#4", "e":"E4", "r":"F4", "5":"F#4", "t":"G4", "6":"G#4", "y":"A4", "7":"A#4", "u":"B4", "i":"C5", "9":"C#5", "o":"D5", "0":"D#5", "p":"E5", "[":"F5", "=":"F#5", "]":"G5", "Backspace":"G#5", "\\":"A5" };
+	// const keyToPitch = {" ":" ", "z":"C3", "s":"C#3", "x":"D3", "d":"D#3", "c":"E3", "v":"F3", "g":"F#3", "b":"G3", "h":"G#3", "n":"A3", "j":"A#3", "m":"B3", ",":"C4", "q":"C4", "2":"C#4", "w":"D4", "3":"D#4", "e":"E4", "r":"F4", "5":"F#4", "t":"G4", "6":"G#4", "y":"A4", "7":"A#4", "u":"B4", "i":"C5", "9":"C#5", "o":"D5", "0":"D#5", "p":"E5", "[":"F5", "=":"F#5", "]":"G5", "Backspace":"G#5", "\\":"A5" };
+	const keyToPitch = {" ":" ", "z":"C2", "s":"C#2", "x":"D2", "d":"D#2", "c":"E2", "v":"F2", "g":"F#2", "b":"G2", "h":"G#2", "n":"A2", "j":"A#2", "m":"B2", ",":"C3", "q":"C3", "2":"C#3", "w":"D3", "3":"D#3", "e":"E3", "r":"F3", "5":"F#3", "t":"G3", "6":"G#3", "y":"A3", "7":"A#3", "u":"B3", "i":"C4", "9":"C#4", "o":"D4", "0":"D#4", "p":"E4", "[":"F4", "=":"F#4", "]":"G4", "Backspace":"G#4", "\\":"A4" };
 	const Tone = window.Tone
-	const synth = new Tone.Synth({}).toMaster()
+
+	const synth = new Tone.Synth({
+		'oscillator': {
+			'type': 'sawtooth'
+		}
+	}).toMaster()
+
 	let down = []
+	let currentNote = ''
 
 	// cannot use let, const, var here. seems to be something about onkeyup being reserved on window object
 	// so let's say you code this as: const onkey -
@@ -18,7 +26,8 @@ const PressQwerty = (props) => {
 	onkeydown = (e) => {
 		if (noteKeys_All.includes(e.key) && !down.some((obj) => obj.key === e.key) ) {
 			down = [...down, { key: e.key, startTime: Tone.Transport.getSecondsAtTime() } ]
-			synth.triggerAttack(keyToPitch[e.key], Tone.context.currentTime)
+			currentNote = keyToPitch[e.key]
+			synth.triggerAttack(currentNote, Tone.context.currentTime)
 		}
 	}
 
@@ -37,11 +46,32 @@ const PressQwerty = (props) => {
 			let noteObject = { note: keyToPitch[e.key], startTime: keyObject.startTime }
 			noteObject.endTime = Tone.Transport.getSecondsAtTime()
 			down = down.filter(obj => obj.key !== e.key)
-			synth.triggerRelease(null)//keyToPitch[e.key])
+			if (currentNote === keyToPitch[e.key]) {
+				synth.triggerRelease(null)//keyToPitch[e.key])
+				// you could build a feature where it returns to play the last previously held note...
+
+					// if (down.length !== 0) {
+					// 	synth.triggerAttack(keyToPitch[down[down.length-1].key])
+					// }
+				// here this triggered note never releases. above, you could test on down[down-1].e.key rather than currentNote...?
+			}
 			console.log(noteObject)
 			addNote(noteObject)
 		}
 	}
+
+	// use cases
+	 	// press q, release q, press w, release w
+
+		// press q, press w, release q, release w
+		// press q, press w, release w, release q
+
+		// press q, press w, press e, release q, release w, release e
+		// press q, press w, press e, release w, release q, release e
+		// press q, press w, press e, release e, release w, release q
+	  // press q, press w, press e, release e, release q, release w
+	  //
+
 
 	useEffect(() => {
 		window.addEventListener('keydown', onkeydown)
