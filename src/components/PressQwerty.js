f21442578743f3e9878169efc40684160cb80d6d
Rob Taussig <robert.taussig@gmail.com>
2019-05-31 22:18:43 -0400

    Change mutable variable to ref, move event handlers to useEffect closure, fix dependencies, use function callback for useState setter

--- src/components/PressQwerty.js
+++ src/components/PressQwerty.js
@@ -1,91 +1,66 @@
 import React from 'react'
-import { useState, useEffect } from 'react'  // what would be the benefit of useState for down vs just having the variable?
+import { useState, useEffect, useRef } from 'react'  // what would be the benefit of useState for down vs just having the variable?
 import { connect } from 'react-redux'
 
 import { synth } from './synth'
 import { addNoteToTune } from '../actions'
 import Qwerty from './Qwerty'
 
-const PressQwerty = (props) => {
+const NOTE_KEYS_ALL = ["a", "z", "s", "x", "d", "c", "f", "v", "g", "b", "h", "n", "j", "m", "k", ",", "l", ".", ";", "/", "'", "1", "q", "2", "w", "3", "4", "e", "r", "5", "t", "6", "y", "7", "u", "8", "i", "9", "o", "0", "p", "-", "[", "=", "]", "Backspace", "\\"]
+// const KEYS_TO_PITCH = {" ":" ", "z":"C3", "s":"C#3", "x":"D3", "d":"D#3", "c":"E3", "v":"F3", "g":"F#3", "b":"G3", "h":"G#3", "n":"A3", "j":"A#3", "m":"B3", ",":"C4", "q":"C4", "2":"C#4", "w":"D4", "3":"D#4", "e":"E4", "r":"F4", "5":"F#4", "t":"G4", "6":"G#4", "y":"A4", "7":"A#4", "u":"B4", "i":"C5", "9":"C#5", "o":"D5", "0":"D#5", "p":"E5", "[":"F5", "=":"F#5", "]":"G5", "Backspace":"G#5", "\\":"A5" };
+const KEYS_TO_PITCH = {" ":" ", "z":"C2", "s":"C#2", "x":"D2", "d":"D#2", "c":"E2", "v":"F2", "g":"F#2", "b":"G2", "h":"G#2", "n":"A2", "j":"A#2", "m":"B2", ",":"C3", "q":"C3", "2":"C#3", "w":"D3", "3":"D#3", "e":"E3", "r":"F3", "5":"F#3", "t":"G3", "6":"G#3", "y":"A3", "7":"A#3", "u":"B3", "i":"C4", "9":"C#4", "o":"D4", "0":"D#4", "p":"E4", "[":"F4", "=":"F#4", "]":"G4", "Backspace":"G#4", "\\":"A4" };
 
-	const noteKeys_All = ["a", "z", "s", "x", "d", "c", "f", "v", "g", "b", "h", "n", "j", "m", "k", ",", "l", ".", ";", "/", "'", "1", "q", "2", "w", "3", "4", "e", "r", "5", "t", "6", "y", "7", "u", "8", "i", "9", "o", "0", "p", "-", "[", "=", "]", "Backspace", "\\"]
-	// const keyToPitch = {" ":" ", "z":"C3", "s":"C#3", "x":"D3", "d":"D#3", "c":"E3", "v":"F3", "g":"F#3", "b":"G3", "h":"G#3", "n":"A3", "j":"A#3", "m":"B3", ",":"C4", "q":"C4", "2":"C#4", "w":"D4", "3":"D#4", "e":"E4", "r":"F4", "5":"F#4", "t":"G4", "6":"G#4", "y":"A4", "7":"A#4", "u":"B4", "i":"C5", "9":"C#5", "o":"D5", "0":"D#5", "p":"E5", "[":"F5", "=":"F#5", "]":"G5", "Backspace":"G#5", "\\":"A5" };
-	const keyToPitch = {" ":" ", "z":"C2", "s":"C#2", "x":"D2", "d":"D#2", "c":"E2", "v":"F2", "g":"F#2", "b":"G2", "h":"G#2", "n":"A2", "j":"A#2", "m":"B2", ",":"C3", "q":"C3", "2":"C#3", "w":"D3", "3":"D#3", "e":"E3", "r":"F3", "5":"F#3", "t":"G3", "6":"G#3", "y":"A3", "7":"A#3", "u":"B3", "i":"C4", "9":"C#4", "o":"D4", "0":"D#4", "p":"E4", "[":"F4", "=":"F#4", "]":"G4", "Backspace":"G#4", "\\":"A4" };
-	const Tone = window.Tone
-
-	// const synth = new Tone.Synth({
-	// 	'oscillator': {
-	// 		'type': 'sawtooth'
-	// 	}
-	// }).toMaster()
-
-	// let down = []
+const PressQwerty = ({ isRecording, addNoteToTune }) => {
 	const [down, setDown] = useState([])
 	const [downKeys, setDownKeys] = useState([])
-	let currentNote = ''
-
-	// cannot use let, const, var here. seems to be something about onkeyup being reserved on window object
-	// so let's say you code this as: const onkey -
-		// a: note how window object has null for onkeydown. also note error - "useEffect has missing dependency onkey"
-		// b: this causes onkeyup's first console.log to have an undefined startTime value, and so the state tune noteObjects don't get startTime!
-	onkeydown = (e) => {
-		console.log(e.key)
-		if (noteKeys_All.includes(e.key) && !down.some((obj) => obj.key === e.key) ) {
-			setDown([...down, { key: e.key, startTime: Tone.Transport.getSecondsAtTime() } ])
-			setDownKeys([...downKeys, e.key])
-			// console.log(downKeys)
-			currentNote = keyToPitch[e.key]
-			synth.triggerAttack(currentNote, Tone.context.currentTime)
-		}
-	}
-
-	// must use let, const or var here.
-	const addNote = (noteObject) => {
-		// this needs to be defined outside the event listener to access current props
-		if (props.isRecording) {
-			props.addNoteToTune(noteObject)
-		}
-	}
-
-	// console.log happens TWICE, even thought the note is only added once...
-	onkeyup = (e) => {
-		if (noteKeys_All.includes(e.key)) {  // && down etc...?
-			let keyObject = {...down.find((obj) => obj.key === e.key)}
-			let noteObject = { note: keyToPitch[e.key], startTime: keyObject.startTime }
-			noteObject.endTime = Tone.Transport.getSecondsAtTime()
-			setDown(down.filter(obj => obj.key !== e.key))
-			setDownKeys(downKeys.filter(key => key !== e.key))
-			if (currentNote === keyToPitch[e.key]) {
-				synth.triggerRelease(null)//keyToPitch[e.key])
-				// you could build a feature where it returns to play the last previously held note...
-
-					// if (down.length !== 0) {
-					// 	synth.triggerAttack(keyToPitch[down[down.length-1].key])
-					// }
-				// here this triggered note never releases. above, you could test on down[down-1].e.key rather than currentNote...?
-			}
-			// console.log(noteObject)
-			addNote(noteObject)
-		}
-	}
-
-	// use cases
-	 	// press q, release q, press w, release w
-
-		// press q, press w, release q, release w
-		// press q, press w, release w, release q
-
-		// press q, press w, press e, release q, release w, release e
-		// press q, press w, press e, release w, release q, release e
-		// press q, press w, press e, release e, release w, release q
-	  // press q, press w, press e, release e, release q, release w
-	  //
-
+	const currentNote = useRef('');
 
 	useEffect(() => {
-		window.addEventListener('keydown', onkeydown)
-		window.addEventListener('keyup', onkeyup)
-	}, [])
+		const addNote = (noteObject) => {
+			// this needs to be defined outside the event listener to access current props
+			if (isRecording) {
+				addNoteToTune(noteObject)
+			}
+		}
+		
+		const handleKeyDown = e => {
+			if (NOTE_KEYS_ALL.includes(e.key) && !down.some((obj) => obj.key === e.key) ) {
+				setDown(prev => prev.concat({ key: e.key, startTime: window.Tone.Transport.getSecondsAtTime() }))
+				setDownKeys(prev => prev.concat(e.key))
+				// console.log(downKeys)
+				currentNote.current = KEYS_TO_PITCH[e.key]
+				synth.triggerAttack(currentNote.current, window.Tone.context.currentTime)
+			}
+		};
+
+		const handleKeyUp = e => {
+			if (NOTE_KEYS_ALL.includes(e.key)) {  // && down etc...?
+				let keyObject = down.find((obj) => obj.key === e.key)
+				let noteObject = { note: KEYS_TO_PITCH[e.key], startTime: keyObject.startTime }
+				noteObject.endTime = window.Tone.Transport.getSecondsAtTime()
+				setDown(prev => prev.filter(obj => obj.key !== e.key))
+				setDownKeys(prev => prev.filter(key => key !== e.key))
+				if (currentNote.current === KEYS_TO_PITCH[e.key]) {
+					synth.triggerRelease(null)//KEYS_TO_PITCH[e.key])
+					// you could build a feature where it returns to play the last previously held note...
+	
+						// if (down.length !== 0) {
+						// 	synth.triggerAttack(KEYS_TO_PITCH[down[down.length-1].key])
+						// }
+					// here this triggered note never releases. above, you could test on down[down-1].e.key rather than currentNote...?
+				}
+				addNote(noteObject)
+			}
+		};
+	
+		window.addEventListener('keydown', handleKeyDown)
+		window.addEventListener('keyup', handleKeyUp)
+
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown)
+			window.removeEventListener('keyup', handleKeyUp)
+		}
+	}, [down, isRecording, addNoteToTune])
 	// note this advice about useEffect, it strangely doesn't seem to apply tho:
 		// https://www.reddit.com/r/reactjs/comments/bv26tc/render_on_key_press/
 		// however it did seem to cause imported synth to get mutiple triggerAttack invocations
@@ -104,8 +79,12 @@ const mapStateToProps = (state) => {
 	}
 }
 
+const mapDispatchToProps = dispatch => ({
+	addNoteToTune: note => dispatch(addNoteToTune(note)),
+});
+
 export default connect(
 	mapStateToProps,
-	{ addNoteToTune }
+	mapDispatchToProps,
 )(PressQwerty);
 
--- src/components/Qwerty.js
+++ src/components/Qwerty.js
@@ -7,10 +7,7 @@ import './qwerty.css';
 // https://codepen.io/gntsketches/pen/GawmLo
 // https://www.reddit.com/r/reactjs/comments/bv26tc/render_on_key_press/
 
-const Qwerty = (props) => {
-
-	const downKeys = props.downKeys
-	// console.log(downKeys)
+const Qwerty = ({ downKeys }) => {
 
 			return (
 			<div id="qwerty" className="qwerty-player" >
@@ -18,68 +15,68 @@ const Qwerty = (props) => {
 				<div id='keyboard'>
 
 					<div className='row' id='row2'>
-						<div className={`key ${downKeys.includes('`') ? 'pressed' : ''}`} id='grave'></div>
-						<div className={`key ${downKeys.includes('1') ? 'pressed' : ''}`} id='1'></div>
-						<div className={`key ${downKeys.includes('2') ? 'pressed' : ''}`} id='2'>C#3</div>
-						<div className={`key ${downKeys.includes('3') ? 'pressed' : ''}`} id='3'>D#3</div>
-						<div className={`key ${downKeys.includes('4') ? 'pressed' : ''}`} id='4'></div>
-						<div className={`key ${downKeys.includes('5') ? 'pressed' : ''}`} id='5'>F#3</div>
-						<div className={`key ${downKeys.includes('6') ? 'pressed' : ''}`} id='6'>G#3</div>
-						<div className={`key ${downKeys.includes('7') ? 'pressed' : ''}`} id='7'>A#3</div>
-						<div className={`key ${downKeys.includes('8') ? 'pressed' : ''}`} id='8'></div>
-						<div className={`key ${downKeys.includes('9') ? 'pressed' : ''}`} id='9'>C#4</div>
-						<div className={`key ${downKeys.includes('0') ? 'pressed' : ''}`} id='0'>D#4</div>
-						<div className={`key ${downKeys.includes('-') ? 'pressed' : ''}`} id='minus'></div>
-						<div className={`key ${downKeys.includes('=') ? 'pressed' : ''}`} id='equals'>F#4</div>
-						<div className={`key ${downKeys.includes('Backspace') ? 'pressed' : ''}`} id='Backspace'>G#4</div>
+						<div className={`key ${pressedClassName(downKeys, '`')}`} id='grave'></div>
+						<div className={`key ${pressedClassName(downKeys, '1')}`} id='1'></div>
+						<div className={`key ${pressedClassName(downKeys, '2')}`} id='2'>C#3</div>
+						<div className={`key ${pressedClassName(downKeys, '3')}`} id='3'>D#3</div>
+						<div className={`key ${pressedClassName(downKeys, '4')}`} id='4'></div>
+						<div className={`key ${pressedClassName(downKeys, '5')}`} id='5'>F#3</div>
+						<div className={`key ${pressedClassName(downKeys, '6')}`} id='6'>G#3</div>
+						<div className={`key ${pressedClassName(downKeys, '7')}`} id='7'>A#3</div>
+						<div className={`key ${pressedClassName(downKeys, '8')}`} id='8'></div>
+						<div className={`key ${pressedClassName(downKeys, '9')}`} id='9'>C#4</div>
+						<div className={`key ${pressedClassName(downKeys, '0')}`} id='0'>D#4</div>
+						<div className={`key ${pressedClassName(downKeys, '-')}`} id='minus'></div>
+						<div className={`key ${pressedClassName(downKeys, '=')}`} id='equals'>F#4</div>
+						<div className={`key ${pressedClassName(downKeys, 'Backspace')}`} id='Backspace'>G#4</div>
 					</div>
 
 					<div className='row' id='row3'>
-						<div className={`key small ${downKeys.includes('Tab') ? 'pressed' : ''}`} id='tab'></div>
-						<div className={`key ${downKeys.includes('q') ? 'pressed' : ''}`} id='q'>C3</div>
-						<div className={`key ${downKeys.includes('w') ? 'pressed' : ''}`} id='w'>D3</div>
-						<div className={`key ${downKeys.includes('e') ? 'pressed' : ''}`} id='e'>E3</div>
-						<div className={`key ${downKeys.includes('r') ? 'pressed' : ''}`} id='r'>F3</div>
-						<div className={`key ${downKeys.includes('t') ? 'pressed' : ''}`} id='t'>G3</div>
-						<div className={`key ${downKeys.includes('y') ? 'pressed' : ''}`} id='y'>A3</div>
-						<div className={`key ${downKeys.includes('u') ? 'pressed' : ''}`} id='u'>B3</div>
-						<div className={`key ${downKeys.includes('i') ? 'pressed' : ''}`} id='i'>C4</div>
-						<div className={`key ${downKeys.includes('o') ? 'pressed' : ''}`} id='o'>D4</div>
-						<div className={`key ${downKeys.includes('p') ? 'pressed' : ''}`} id='p'>E4</div>
-						<div className={`key ${downKeys.includes('[') ? 'pressed' : ''}`} id='openbracket'>F4</div>
-						<div className={`key ${downKeys.includes(']') ? 'pressed' : ''}`} id='closebracket'>G4</div>
-						<div className={`key ${downKeys.includes('\\') ? 'pressed' : ''}`} id='backslash'>A4</div>
+						<div className={`key small ${pressedClassName(downKeys, 'Tab')}`} id='tab'></div>
+						<div className={`key ${pressedClassName(downKeys, 'q')}`} id='q'>C3</div>
+						<div className={`key ${pressedClassName(downKeys, 'w')}`} id='w'>D3</div>
+						<div className={`key ${pressedClassName(downKeys, 'e')}`} id='e'>E3</div>
+						<div className={`key ${pressedClassName(downKeys, 'r')}`} id='r'>F3</div>
+						<div className={`key ${pressedClassName(downKeys, 't')}`} id='t'>G3</div>
+						<div className={`key ${pressedClassName(downKeys, 'y')}`} id='y'>A3</div>
+						<div className={`key ${pressedClassName(downKeys, 'u')}`} id='u'>B3</div>
+						<div className={`key ${pressedClassName(downKeys, 'i')}`} id='i'>C4</div>
+						<div className={`key ${pressedClassName(downKeys, 'o')}`} id='o'>D4</div>
+						<div className={`key ${pressedClassName(downKeys, 'p')}`} id='p'>E4</div>
+						<div className={`key ${pressedClassName(downKeys, '[')}`} id='openbracket'>F4</div>
+						<div className={`key ${pressedClassName(downKeys, ']')}`} id='closebracket'>G4</div>
+						<div className={`key ${pressedClassName(downKeys, '\\')}`} id='backslash'>A4</div>
 					</div>
 
 					<div className='row' id='row4'>
-						<div className={`key small ${downKeys.includes('CapsLock') ? 'pressed' : ''}`} id='capslock'></div>
-						<div className={`key ${downKeys.includes('a') ? 'pressed' : ''}`} id='a'></div>
-						<div className={`key ${downKeys.includes('s') ? 'pressed' : ''}`} id='s'>C#2</div>
-						<div className={`key ${downKeys.includes('d') ? 'pressed' : ''}`} id='d'>D#2</div>
-						<div className={`key ${downKeys.includes('f') ? 'pressed' : ''}`} id='f'></div>
-						<div className={`key ${downKeys.includes('g') ? 'pressed' : ''}`} id='g'>F#2</div>
-						<div className={`key ${downKeys.includes('h') ? 'pressed' : ''}`} id='h'>G#2</div>
-						<div className={`key ${downKeys.includes('j') ? 'pressed' : ''}`} id='j'>A#2</div>
-						<div className={`key ${downKeys.includes('k') ? 'pressed' : ''}`} id='k'></div>
-						<div className={`key ${downKeys.includes('l') ? 'pressed' : ''}`} id='l'>C#3</div>
-						<div className={`key ${downKeys.includes(';') ? 'pressed' : ''}`} id='semicolon'>D#3</div>
-						<div className={`key ${downKeys.includes("'") ? 'pressed' : ''}`} id='apostrophe'>F3</div>
-						<div className={`key small ${downKeys.includes('Enter') ? 'pressed' : ''}`} id='enter'></div>
+						<div className={`key small ${pressedClassName(downKeys, 'CapsLock')}`} id='capslock'></div>
+						<div className={`key ${pressedClassName(downKeys, 'a')}`} id='a'></div>
+						<div className={`key ${pressedClassName(downKeys, 's')}`} id='s'>C#2</div>
+						<div className={`key ${pressedClassName(downKeys, 'd')}`} id='d'>D#2</div>
+						<div className={`key ${pressedClassName(downKeys, 'f')}`} id='f'></div>
+						<div className={`key ${pressedClassName(downKeys, 'g')}`} id='g'>F#2</div>
+						<div className={`key ${pressedClassName(downKeys, 'h')}`} id='h'>G#2</div>
+						<div className={`key ${pressedClassName(downKeys, 'j')}`} id='j'>A#2</div>
+						<div className={`key ${pressedClassName(downKeys, 'k')}`} id='k'></div>
+						<div className={`key ${pressedClassName(downKeys, 'l')}`} id='l'>C#3</div>
+						<div className={`key ${pressedClassName(downKeys, ';')}`} id='semicolon'>D#3</div>
+						<div className={`key ${pressedClassName(downKeys, "'")}`} id='apostrophe'>F3</div>
+						<div className={`key small ${pressedClassName(downKeys, 'Enter')}`} id='enter'></div>
 					</div>
 
 					<div className='row' id='row5'>
-						<div className={`key shift small ${downKeys.includes('Shift') ? 'pressed' : ''}`} id='lshift'></div>
-						<div className={`key ${downKeys.includes('z') ? 'pressed' : ''}`} id='z'>C2</div>
-						<div className={`key ${downKeys.includes('x') ? 'pressed' : ''}`} id='x'>D2</div>
-						<div className={`key ${downKeys.includes('c') ? 'pressed' : ''}`} id='c'>E2</div>
-						<div className={`key ${downKeys.includes('v') ? 'pressed' : ''}`} id='v'>F2</div>
-						<div className={`key ${downKeys.includes('b') ? 'pressed' : ''}`} id='b'>G2</div>
-						<div className={`key ${downKeys.includes('n') ? 'pressed' : ''}`} id='n'>A2</div>
-						<div className={`key ${downKeys.includes('m') ? 'pressed' : ''}`} id='m'>B2</div>
-						<div className={`key ${downKeys.includes(',') ? 'pressed' : ''}`} id='comma'>C3</div>
-						<div className={`key ${downKeys.includes('.') ? 'pressed' : ''}`} id='period'>D3</div>
-						<div className={`key ${downKeys.includes('/') ? 'pressed' : ''}`} id='slash'>E3</div>
-						<div className={`key shift small ${downKeys.includes('Shift') ? 'pressed' : ''}`} id='rshift'></div>
+						<div className={`key shift small ${pressedClassName(downKeys, 'Shift')}`} id='lshift'></div>
+						<div className={`key ${pressedClassName(downKeys, 'z')}`} id='z'>C2</div>
+						<div className={`key ${pressedClassName(downKeys, 'x')}`} id='x'>D2</div>
+						<div className={`key ${pressedClassName(downKeys, 'c')}`} id='c'>E2</div>
+						<div className={`key ${pressedClassName(downKeys, 'v')}`} id='v'>F2</div>
+						<div className={`key ${pressedClassName(downKeys, 'b')}`} id='b'>G2</div>
+						<div className={`key ${pressedClassName(downKeys, 'n')}`} id='n'>A2</div>
+						<div className={`key ${pressedClassName(downKeys, 'm')}`} id='m'>B2</div>
+						<div className={`key ${pressedClassName(downKeys, ',')}`} id='comma'>C3</div>
+						<div className={`key ${pressedClassName(downKeys, '.')}`} id='period'>D3</div>
+						<div className={`key ${pressedClassName(downKeys, '/')}`} id='slash'>E3</div>
+						<div className={`key shift small ${pressedClassName(downKeys, 'Shift')}`} id='rshift'></div>
 					</div>
 
 					{/*<div className='row' id='row6'>*/}
@@ -92,15 +89,10 @@ const Qwerty = (props) => {
 						{/*<div className='key small' id='menu'></div>*/}
 						{/*<div className='ctrl key small' id='rctrl'>ctrl</div>*/}
 					{/*</div>*/}
-
 				</div>
-
-
-
 			</div>
-
-		)
-
+		);
 }
 
+const pressedClassName = (downKeys, char) => downKeys.includes(char) ? 'pressed' : '';
 export default Qwerty;
